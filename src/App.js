d2824ad3b2b731a0924264883d73ccd8b39d86d6
Daniel Assumpção Bueno <80775444+Danera1903@users.noreply.github.com>
2026-01-27 16:31:04 -0300

    fix: simplify pie chart label

--- src/App.js
+++ src/App.js
@@ -316,7 +316,7 @@ const CSVAnalyzer = () => {
                         cx="50%"
                         cy="50%"
                         labelLine={false}
-                        label={(entry) => `${entry.name}: ${(entry.percent * 100).toFixed(0)}%`}
+                        label={({ name, percent }) => `${name} ${(percent * 100).toFixed(0)}%`}
                         outerRadius={80}
                         fill="#8884d8"
                         dataKey="value"
