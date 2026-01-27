ea31fb9b8e827be71710821e820137e494a265f9
Daniel Assumpção Bueno <80775444+Danera1903@users.noreply.github.com>
2026-01-27 16:33:50 -0300

    fix: remove pie chart labels to fix build error

--- src/App.js
+++ src/App.js
@@ -311,12 +311,10 @@ const CSVAnalyzer = () => {
                   </h3>
                   <ResponsiveContainer width="100%" height={300}>
                     <PieChart>
-                      <Pie
+                     <Pie
                         data={chartData}
                         cx="50%"
                         cy="50%"
-                        labelLine={false}
-                        label={({ name, percent }) => `${name} ${(percent * 100).toFixed(0)}%`}
                         outerRadius={80}
                         fill="#8884d8"
                         dataKey="value"
